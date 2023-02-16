27d24fdd6deccf7d2da61c915b670309e0bf83af
Ashlyn Carmosino <ashlyncarmosino@Ashlyns-Air.lan>
2023-02-16 14:45:23 -0500

    exercise 2

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -9,14 +9,38 @@ namespace CarLotSimulator
             //TODO
 
             //Create a seperate class file called Car
+
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
-
+            //DONE
 
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            //Dot notation
+            Car c1 = new Car();
+            c1.Make = "Ford";
+            c1.Model = "F-150";
+            c1.Year = 2015;
+            c1.EngineNoise = "VROOM VROOM";
+            c1.HonkNoise = "HONK HONK";
+            c1.IsDrivable = true;
+
+            //object initalizer
+            Car c2 = new Car()
+            {
+                Make = "Chevy",
+                Model = "Avalanche",
+                Year = 2004,
+                EngineNoise = "brum brum",
+                HonkNoise = "Beep Beep",
+                IsDrivable = true,
+            };
+
+            //Custom Constructor
+            Car c3 = new Car(2022, "Ford", "Mustang");
+
 
             //*************BONUS*************//
 
